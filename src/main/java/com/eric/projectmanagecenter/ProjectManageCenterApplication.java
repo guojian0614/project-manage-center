dfd26f8ef721cecf28278c4b8d64c1342fd5d705
guojian <guojian@365ime.com>
2018-10-20 19:38:04 +0800

    fist commit

---
+++ src/main/java/com/eric/projectmanagecenter/ProjectManageCenterApplication.java
@@ -0,0 +1,12 @@
+package com.eric.projectmanagecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManageCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManageCenterApplication.class, args);
+    }
+}
